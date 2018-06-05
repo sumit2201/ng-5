44690668857bacad5e663e4a11a88c93163480c7
sumit2201 <sumitneema1988@gmail.com>
2018-06-05 19:48:01 +0530

    migrate to ng-6

--- src/app/home/title/title.service.ts
+++ src/app/home/title/title.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class Title {
 
-  public value = 'Angular 2';
+  public value = "The Angular";
 
   constructor(
     public http: HttpClient
   ) { }
 
   public getData() {
-    console.log('Title#getData(): Get Data');
-    return this.http.get('/assets/data.json');
+    console.log("Title#getData(): Get Data");
+    return this.http.get("/assets/data.json");
   }
 
 }
